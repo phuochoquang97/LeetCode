a5fe7199612d5f52d94b7340b0002fd7f9167ff9
Phuoc Ho <phuochoquang97@gmail.com>
2024-09-26 16:52:25 +0200

    can be improved to O(logN) - OK

---
+++ 729_Medium_My Calendar I.py
@@ -0,0 +1,34 @@
+class MyCalendar:
+
+    def __init__(self):
+        self.time_line = []
+
+    def book(self, start: int, end: int) -> bool:
+        if not self.time_line:
+            self.time_line.append((start, end))
+            # print(self.time_line)
+            return True
+        i = 0
+        while i < len(self.time_line) and self.time_line[i][1] <= start:
+            i += 1
+        # print(f"i: {i}")
+        if i == 0:
+            if end <= self.time_line[0][0]:
+                self.time_line.insert(0, (start, end))
+                # print(self.time_line)
+                return True
+        elif i == len(self.time_line):
+            self.time_line.append((start, end))
+            # print(self.time_line)
+            return True
+        elif self.time_line[i - 1][1] <= start and end <= self.time_line[i][0]:
+            self.time_line.insert(i, (start, end))
+            # print(self.time_line)
+            return True
+
+        return False
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
