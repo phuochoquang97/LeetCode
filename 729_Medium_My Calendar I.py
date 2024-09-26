1c15492fcff8eee8c27c537f1588bc633513488f
Phuoc Ho <phuochoquang97@gmail.com>
2024-09-26 17:02:06 +0200

    binary search, still O(N), can be improved - OK

--- 729_Medium_My Calendar I.py
+++ 729_Medium_My Calendar I.py
@@ -2,16 +2,20 @@ class MyCalendar:
 
     def __init__(self):
         self.time_line = []
+        self.second_element = []
 
     def book(self, start: int, end: int) -> bool:
         if not self.time_line:
             self.time_line.append((start, end))
             # print(self.time_line)
             return True
-        i = 0
-        while i < len(self.time_line) and self.time_line[i][1] <= start:
-            i += 1
+        # i = 0
+        # while  i < len(self.time_line) and self.time_line[i][1] <= start:
+        # i += 1
+        second_element = [t[1] for t in self.time_line]
+        i = bisect.bisect_right(second_element, start)
         # print(f"i: {i}")
+
         if i == 0:
             if end <= self.time_line[0][0]:
                 self.time_line.insert(0, (start, end))
