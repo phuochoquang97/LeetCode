8f4212db4b95aa38467fcdcc5441681aab5876d9
Phuoc Ho <phuochoquang97@gmail.com>
2024-10-11 11:29:33 +0200

    962 - monostack - OK

--- 962_Medium_Maximum Width Ramp.py
+++ 962_Medium_Maximum Width Ramp.py
@@ -1,29 +1,18 @@
 class Solution:
     def maxWidthRamp(self, nums: List[int]) -> int:
-        # find largest j - i with nums[j] >= nums[i]
-        # bruteforce -> O(n^2) TLE
-
+        stack = []
         n = len(nums)
-        # left min array
-        left_min = [0] * n
-        left_min[0] = nums[0]
-        for i in range(1, n):
-            left_min[i] = min(left_min[i - 1], nums[i])
-
-        # right max array
-        right_max = [0] * n
-        right_max[n - 1] = nums[n - 1]
-        for i in range(n - 2, -1, -1):
-            right_max[i] = max(right_max[i + 1], nums[i])
-
-        i, j = 0, 0
-        max_diff = -1
-        n = len(nums)
-        while i < n and j < n:
-            if left_min[i] <= right_max[j]:
-                max_diff = max(max_diff, j - i)
-                j += 1
+        for i in range(n):
+            if not stack:
+                stack.append(i)
             else:
-                i += 1
+                if nums[i] < nums[stack[-1]]:
+                    stack.append(i)
+
+        max_diff = -1
+        for j in range(n - 1, -1, -1):
+            while stack and nums[j] >= nums[stack[-1]]:
+                max_diff = max(max_diff, j - stack[-1])
+                stack.pop()
 
         return max_diff
